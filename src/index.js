36a0689451bfd8ebb2c8a0dfc3a100a183ec48ad
Bumble <bumble.sakh@gmail.com>
2022-12-12 01:52:11 +1100

    feat: add start app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { app } from './app';
+import { app } from './app.js';
 
-app();
+const args = process.argv.splice(2);
+
+if (args.length > 0 && args[0].startsWith('--username') && args[0].includes('=')) {
+  const [_, userName] = args[0].split('=');
+
+  app(userName);
+} else {
+  console.log("For start program type: 'npm run start -- --username=your_username'");
+}
