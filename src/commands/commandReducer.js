2bfa1bff5403efc47e397bd1a4d34f2c81593a3b
Bumble <bumble.sakh@gmail.com>
2022-12-12 01:53:48 +1100

    feat: add command reducer

---
+++ src/commands/commandReducer.js
@@ -0,0 +1,77 @@
+import store from '../store/store.js';
+import { stdin } from 'process';
+import { COMMANDS } from '../constants/commands.js';
+import { up } from './nwd/up.js';
+import { cd } from './nwd/cd.js';
+import { ls } from './nwd/ls.js';
+import { os } from './os/os.js';
+import { OS_ARGUMENTS } from '../constants/osArguments.js';
+import { hash } from './hash/hash.js';
+
+export const commandReducer = async ({ command, payload }) => {
+  switch (command) {
+    case COMMANDS.up:
+      if (payload.length > 0) {
+        stdin.emit('operationFailed');
+        break;
+      }
+
+      up();
+
+      console.log(`You are currently in ${store.currentPath}`);
+      break;
+
+    case COMMANDS.cd:
+      if (payload.length !== 1) {
+        stdin.emit('operationFailed');
+        break;
+      }
+
+      const [destinationPath] = payload;
+      await cd(destinationPath);
+
+      console.log(`You are currently in ${store.currentPath}`);
+      break;
+
+    case COMMANDS.ls:
+      if (payload.length > 0) {
+        stdin.emit('operationFailed');
+        break;
+      }
+
+      await ls();
+
+      console.log(`You are currently in ${store.currentPath}`);
+      break;
+
+    case COMMANDS.os:
+      if (payload.length !== 1) {
+        stdin.emit('operationFailed');
+        break;
+      }
+
+      const argument = payload[0].slice(2);
+      const osArgument = OS_ARGUMENTS[argument];
+
+      os(osArgument);
+
+      console.log(`You are currently in ${store.currentPath}`);
+      break;
+
+    case COMMANDS.hash:
+      if (payload.length !== 1) {
+        stdin.emit('operationFailed');
+        break;
+      }
+
+      await hash(...payload);
+
+      console.log(`You are currently in ${store.currentPath}`);
+      break;
+
+    default:
+      console.log('Invalid input');
+      console.log(`You are currently in ${store.currentPath}`);
+      break;
+  }
+};
