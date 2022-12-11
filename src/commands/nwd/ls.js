09db91431a6a4680d0329bf6481179dc730e1b6d
Bumble <bumble.sakh@gmail.com>
2022-12-12 01:55:31 +1100

    feat: add ls command implementation

---
+++ src/commands/nwd/ls.js
@@ -0,0 +1,35 @@
+import { readdir } from 'fs/promises';
+import store from '../../store/store.js';
+
+export const ls = async () => {
+  try {
+    const files = await readdir(store.currentPath, { withFileTypes: true });
+    const result = files.map((file) => {
+      return { name: file.name, type: file.isFile() ? 'file' : 'directory' };
+    });
+
+    result
+      .sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return -1;
+        }
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      })
+      .sort((a, b) => {
+        if (a.type < b.type) {
+          return -1;
+        }
+        if (a.type > b.type) {
+          return 1;
+        }
+        return 0;
+      });
+
+    console.table(result);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
