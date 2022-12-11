321e76e03f3fa8dd9395a920b323bff6efd6486a
Bumble <bumble.sakh@gmail.com>
2022-12-12 01:54:02 +1100

    feat: add app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,36 @@
-export const app = () => {
-  // App
+import store from './store/store.js';
+
+import { stdin } from 'process';
+import { commandReducer } from './commands/commandReducer.js';
+import { COMMANDS } from './constants/commands.js';
+
+export const app = (userName) => {
+  console.log(`Welcome to the File Manager, ${userName}!`);
+  console.log(`You are currently in ${store.startPath}`);
+
+  stdin.read();
+
+  stdin.on('command', async (command) => {
+    await commandReducer(command);
+  });
+
+  stdin.on('operationFailed', () => {
+    console.log('Operation failed');
+  });
+
+  stdin.on('data', (data) => {
+    const [command, ...payload] = data.toString().trim().split(' ');
+
+    stdin.emit('command', { command: COMMANDS[command], payload });
+
+    if (command == '.exit') {
+      console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+      process.exit(0);
+    }
+  });
+
+  process.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit(0);
+  });
 };
