213d3b7c98ab053832a2f1ed103a2c29c6fda8b3
Bumble <bumble.sakh@gmail.com>
2022-12-14 18:19:04 +1100

    refactor: check existence newFile

--- src/commands/files/rn.js
+++ src/commands/files/rn.js
@@ -7,6 +7,8 @@ export const rn = async (filePath, fileName) => {
   const newPath = path.resolve(store.currentPath, fileName);
 
   try {
+    const file = await fs.open(newPath, 'wx');
+    file.close();
     await fs.rename(oldPath, newPath);
   } catch (error) {
     console.log(error.message);
