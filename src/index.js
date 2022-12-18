4cbea2df643fa6bff462edfa28c98db179da1183
Bumble <bumble.sakh@gmail.com>
2022-12-18 18:48:53 +1100

    feat: add Anonymous

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const args = process.argv.splice(2);
 if (args.length > 0 && args[0].startsWith('--username') && args[0].includes('=')) {
   const [_, userName] = args[0].split('=');
 
-  app(userName);
+  app(userName ? userName : 'Anonymous');
 } else {
   console.log("For start program type: 'npm run start -- --username=your_username'");
 }
